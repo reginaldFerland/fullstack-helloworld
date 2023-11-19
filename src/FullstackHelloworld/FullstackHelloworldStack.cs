d72a65ca19c4ab4a265d3c3f6c2d8274fa19b227
Reginald Ferland <reginald@ferland.io>
2023-11-19 09:02:32 -0500

    Create a VPC for app

--- src/FullstackHelloworld/FullstackHelloworldStack.cs
+++ src/FullstackHelloworld/FullstackHelloworldStack.cs
@@ -1,4 +1,5 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.EC2;
 using Constructs;
 
 namespace FullstackHelloworld
@@ -8,6 +9,40 @@ namespace FullstackHelloworld
         internal FullstackHelloworldStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
             // The code that defines your stack goes here
+
+            // TODO: Create VPC
+            var vpc = new Vpc(this, "VPC", new VpcProps
+            {
+                IpAddresses = IpAddresses.Cidr("10.0.0.0/16"),
+                ReservedAzs = 2,
+                SubnetConfiguration = new[]
+                {
+                    new SubnetConfiguration
+                    {
+                        CidrMask = 24,
+                        Name = "public",
+                        SubnetType = SubnetType.PUBLIC
+                    },
+                    new SubnetConfiguration {
+                        CidrMask = 24,
+                        Name = "private",
+                        SubnetType = SubnetType.PRIVATE_WITH_EGRESS,
+                    },
+                    new SubnetConfiguration {
+                        CidrMask = 20,
+                        Name = "Isolated",
+                        SubnetType = SubnetType.PRIVATE_ISOLATED,
+                    },
+                } 
+            });
+
+            // TODO: Create ECR
+
+            // TODO: Create ECS Cluster
+
+            // TODO: Create ECS Task 
+
+            // TODO: Create ECS Service
         }
     }
 }
